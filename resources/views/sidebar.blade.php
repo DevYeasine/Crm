70bc02d826f720a88e8bdc2b39db3139989b79a0
DevYeasin <yeasinhassan661@gmail.com>
2025-08-28 23:03:05 -0700

    Created Necessary Files, Models, Migrations, Controllers

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -3,146 +3,69 @@
         <nav class="sidebar-nav">
             <ul class="nav flex-column">
                 <li class="nav-item">
-                    <a class="nav-link active" href="{{ asset('./index.html') }}">
+                    <a class="nav-link active" href="{{ route('dashboard.index') }}">
                         <i class="bi bi-speedometer2"></i>
                         <span>Dashboard</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ asset('./analytics.html') }}">
+                    <a class="nav-link" href="{{ route('leads.index') }}">
                         <i class="bi bi-graph-up"></i>
-                        <span>Analytics</span>
+                        <span>Leads</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ asset('./users.html') }}">
+                    <a class="nav-link" href="{{ route('deals.index') }}">
                         <i class="bi bi-people"></i>
-                        <span>Users</span>
+                        <span>Deals</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ asset('./products.html') }}">
+                    <a class="nav-link" href="{{ route('projects.index') }}">
                         <i class="bi bi-box"></i>
-                        <span>Products</span>
+                        <span>Projects</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ asset('./orders.html') }}">
+                    <a class="nav-link" href="{{ route('contacts.index') }}">
                         <i class="bi bi-bag-check"></i>
-                        <span>Orders</span>
+                        <span>Contacts</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ asset('./forms.html') }}">
+                    <a class="nav-link" href="{{ route('tasks.index') }}">
                         <i class="bi bi-ui-checks"></i>
-                        <span>Forms</span>
-                        <span class="badge bg-success rounded-pill ms-auto">New</span>
+                        <span>Tasks</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ asset('#') }}" data-bs-toggle="collapse" data-bs-target="#elementsSubmenu" aria-expanded="false">
-                        <i class="bi bi-puzzle"></i>
-                        <span>Elements</span>
-                        <span class="badge bg-primary rounded-pill me-2">New</span>
-                        <i class="bi bi-chevron-down ms-auto"></i>
-                    </a>
-                    <div class="collapse" id="elementsSubmenu">
-                        <ul class="nav nav-submenu">
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ asset('./elements.html') }}">
-                                    <i class="bi bi-grid"></i>
-                                    <span>Overview</span>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ asset('./elements-buttons.html') }}">
-                                    <i class="bi bi-square"></i>
-                                    <span>Buttons</span>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ asset('./elements-alerts.html') }}">
-                                    <i class="bi bi-exclamation-triangle"></i>
-                                    <span>Alerts</span>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ asset('./elements-badges.html') }}">
-                                    <i class="bi bi-award"></i>
-                                    <span>Badges</span>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ asset('./elements-cards.html') }}">
-                                    <i class="bi bi-card-text"></i>
-                                    <span>Cards</span>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ asset('./elements-modals.html') }}">
-                                    <i class="bi bi-window"></i>
-                                    <span>Modals</span>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ asset('./elements-forms.html') }}">
-                                    <i class="bi bi-ui-checks"></i>
-                                    <span>Forms</span>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ asset('./elements-tables.html') }}">
-                                    <i class="bi bi-table"></i>
-                                    <span>Tables</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ asset('./reports.html') }}">
-                        <i class="bi bi-file-earmark-text"></i>
-                        <span>Reports</span>
+                    <a class="nav-link" href="{{ route('events.index') }}">
+                        <i class="bi bi-ui-checks"></i>
+                        <span>Events</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ asset('./messages.html') }}">
-                        <i class="bi bi-chat-dots"></i>
-                        <span>Messages</span>
-                        <span class="badge bg-danger rounded-pill ms-auto">3</span>
+                    <a class="nav-link" href="{{ route('emails.index') }}">
+                        <i class="bi bi-ui-checks"></i>
+                        <span>Emails</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ asset('./calendar.html') }}">
-                        <i class="bi bi-calendar-event"></i>
-                        <span>Calendar</span>
+                    <a class="nav-link" href="{{ route('automations.index') }}">
+                        <i class="bi bi-ui-checks"></i>
+                        <span>Automation</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ asset('./files.html') }}">
-                        <i class="bi bi-folder2-open"></i>
-                        <span>Files</span>
-                    </a>
-                </li>
-                <li class="nav-item mt-3">
-                    <small class="text-muted px-3 text-uppercase fw-bold">Admin</small>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ asset('./settings.html') }}">
-                        <i class="bi bi-gear"></i>
-                        <span>Settings</span>
+                    <a class="nav-link" href="{{ route('integrations.index') }}">
+                        <i class="bi bi-ui-checks"></i>
+                        <span>Integrations</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ asset('./security.html') }}">
-                        <i class="bi bi-shield-check"></i>
-                        <span>Security</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ asset('./help.html') }}">
-                        <i class="bi bi-question-circle"></i>
-                        <span>Help & Support</span>
+                    <a class="nav-link" href="{{ route('team-members.index') }}">
+                        <i class="bi bi-ui-checks"></i>
+                        <span>Team Members</span>
                     </a>
                 </li>
             </ul>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,33 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{
+    AutomationsController,
+    ContactsController,
+    DashboardController,
+    LeadsController,
+    DealsController,
+    ProjectsController,
+    TasksController,
+    EventsController,
+    EmailsController,
+    IntegrationsController,
+    TeamMembersController
+};
 
 Route::get('/', function () {
-    return view('dashboard');
+    return view('welcome');
 });
 
+Route::resource('/dashboard', DashboardController::class);
+Route::resource('/leads', LeadsController::class);
+Route::resource('/deals', DealsController::class);
+Route::resource('/projects', ProjectsController::class);
+Route::resource('/contacts', ContactsController::class);
+Route::resource('/tasks', TasksController::class);
+Route::resource('/events', EventsController::class);
+Route::resource('/emails', EmailsController::class);
+Route::resource('/automations', AutomationsController::class);
+Route::resource('/integrations', IntegrationsController::class);
+Route::resource('/team-members', TeamMembersController::class);
 
-Route::get('/deals', function () {
-    return view('deals');
-});
